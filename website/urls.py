38e7d4635f3ab02d4efaf35737662004115f379b
Hossein <139358833+HosseinnFazlii@users.noreply.github.com>
2023-11-10 11:21:17 +0330

    get ready for statics file

---
+++ website/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path , include
+from website.views import index_view,about_view
+
+#app_name='website'
+
+urlpatterns = [
+    
+    path('',index_view,name='index'),
+    path('about',about_view,name='about')
+]
